c886f67fc50a4691d8f14d18474cf62d252dcdf8
Tom Draper <tomjdraper1@gmail.com>
2022-06-17 15:38:07 +0100

    remove unused

--- cleaner.py
+++ cleaner.py
@@ -1,11 +1,8 @@
-import enum
 import sys
 import os
 import re
 
-def html_files():
-    valid_extensions = {'html', 'svelte', 'vue'}
-    
+def collect_files(extensions):    
     # Get list of all files
     all_files = []
     for root, dirs, files in os.walk('./'):
@@ -15,26 +12,29 @@ def html_files():
     # Remove invalid files
     for i in range(len(all_files)-1, -1, -1):
         ext = all_files[i].split('.')[-1]
-        if ext not in valid_extensions:
+        if ext not in extensions:
             all_files.pop(i)
         
     return all_files
-    
-def run(path):
-    styles = set()
-    with open(path, 'r') as f:
-        css = f.read()
-        for style_tag in re.findall(r'\n([^:\n@%]*)\{', css):
-            styles.add(style_tag.strip())
-    print(len(styles), 'styles found')
-            
-    
-    files = html_files()
 
-    for file_path in files:
+def extract_css_styles(css):
+    styles = set()
+    for style_tag in re.findall(r'\n([^:\n@%]*)\{', css):
+        styles = styles.union(set(style_tag.strip().split(' ')))
+    print(len(styles), 'styles found')
+    return styles
+    
+
+def remove_unused(css):
+    styles = extract_css_styles(css)
+    
+    html_files = collect_files({'html', 'svelte', 'vue'})
+
+    # Search through all classes and ids used across all HTML and remove used
+    for file_path in html_files:
         with open(file_path, 'r') as f:
             html = f.read()
-            # Cover classes
+            # Remove used classes
             for classes in re.findall(r'class="(.*)"', html):
                 for _class in classes.split(' '):
                     try:
@@ -42,16 +42,43 @@ def run(path):
                     except:
                         pass
             
-            # Cover ids
+            # Remove used ids
             for ids in re.findall(r'id="(.*)"', html):
                 for _id in ids.split(' '):
                     try:
                         styles.remove('#' + _id)
                     except:
                         pass
+                    
+            # HTML tags
+            to_remove = set()
+            for style in styles:
+                if '.' not in style and '#' not in style and '<' + style in html:
+                    to_remove.add(style)
+            styles = styles - to_remove
     
+    # CSS classes may be added using javascript
+    js_files = collect_files({'js', 'mjs', 'svelte', 'vue', 'html'})
+    to_remove = set()
+    for file_path in js_files:
+        with open(file_path, 'r') as f:
+            js = f.read()
+            # Any mention of style within js -> assume style used and added using js
+            for style in styles:
+                if style.replace('.', '').replace('#', '') in js:
+                    to_remove.add(style)
+    styles = styles - to_remove
+    
+    print('Ignoring styles mentioned in JavaScript', to_remove) 
+       
     # Styles remaining in set are not found in any html file
     print('To remove', len(styles), 'styles:', styles)
+    
+def run(path):
+    with open(path, 'r') as f:
+        css = f.read()
+    
+    remove_unused(css)
 
 def get_path():
     if len(sys.argv) > 1:
